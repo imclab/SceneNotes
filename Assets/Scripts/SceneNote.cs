816a8c3bf3c8eb80340364b082d264c37028bb71
Michael Stevenson <michael@mstevenson.net>
2013-04-21 23:17:17 -0700

    Refactoring SceneNote not to be a Monobehaviour

--- Assets/Scripts/SceneNote.cs
+++ Assets/Scripts/SceneNote.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
 using System.Collections;
+using System.Xml.Serialization;
 
-public class SceneNote : MonoBehaviour {
-	
-	[Multiline]
+public class SceneNote : IXmlSerializable {
+
+	public Vector3 position;
 	public string text = "";
+
+	public System.Xml.Schema.XmlSchema GetSchema ()
+	{
+		return null;
+	}
+
+	public void ReadXml (System.Xml.XmlReader reader)
+	{
+//		reader.ReadElementString ("Text", text);
+//		reader.ReadStartElement ("Position");
+//		Vector3 pos = new Vector3 ();
+//		pos.x = float.Parse (reader.ReadAttributeValue ("x"));
+//		pos.y = float.Parse (reader.ReadAttributeValue ("y"));
+//		pos.z = float.Parse (reader.ReadAttributeValue ("z"));
+//		position = pos;
+//		reader.ReadEndElement ();
+	}
+
+	public void WriteXml (System.Xml.XmlWriter writer)
+	{
+//		writer.WriteElementString ("Text", text);
+//		writer.WriteStartElement ("Position");
+//		writer.WriteAttributeString ("x", position.x);
+//		writer.WriteAttributeString ("y", position.y);
+//		writer.WriteAttributeString ("x", position.z);
+//		writer.WriteEndElement ();
+	}
 }
--- Assets/Scripts/SceneNoteManager.cs
+++ Assets/Scripts/SceneNoteManager.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
 
 public class SceneNoteManager : MonoBehaviour {
 	
+	public Transform player;
+
 	SceneNote currentNote;
-	
+
+	public List<SceneNote> notes = new List<SceneNote> ();
+
 	void ViewNote (SceneNote note)
 	{
 		currentNote = note;
@@ -33,12 +40,35 @@ public class SceneNoteManager : MonoBehaviour {
 	
 	void CreateNoteAtPosition (Vector3 position)
 	{
-		GameObject go = new GameObject ("Note");
-		go.transform.position = position;
-		var note = go.AddComponent<SceneNote> ();
-		currentNote = note;
+		SceneNote note = new SceneNote ();
+		note.position = position;
+		notes.Add (note);
 	}
-	
+
+
+	SceneNote FindNearestNoteInView (Camera cam)
+	{
+		// Frustum check
+		List<SceneNote> inView = new List<SceneNote> ();
+		foreach (var note in notes) {
+			var p = cam.WorldToViewportPoint (note.position);
+			if (p.x < 1 && p.x > 0 && p.y < 1 && p.x > 0 && p.z > 0 && p.z < 1) {
+				inView.Add (note);
+			}
+		}
+		// Distance check
+		float dist = Mathf.Infinity;
+		SceneNote nearest = null;
+		foreach (var note in inView) {
+			float newDist = Vector3.SqrMagnitude (note.position - player.transform.position);
+			if (newDist < dist) {
+				dist = newDist;
+				nearest = note;
+			}
+		}
+		return nearest;
+	}
+
 	
 	void OnGUI ()
 	{
@@ -47,15 +77,20 @@ public class SceneNoteManager : MonoBehaviour {
 				currentNote = null;
 				GUI.FocusControl (null);
 			} else {
-				var pos = WorldToGUIPoint (currentNote.transform.position);
-				var r = new Rect (pos.x, pos.y, 100, 200);
-				GUI.BeginGroup (r, (GUIStyle)"box");
-				GUI.SetNextControlName ("note");
-				currentNote.text = GUILayout.TextArea (currentNote.text, GUILayout.Width (100), GUILayout.Height (200));
-				GUI.FocusControl ("note");
-				GUI.EndGroup ();
+//				var pos = WorldToGUIPoint (currentNote.transform.position);
+//				var r = new Rect (pos.x, pos.y, 100, 200);
+//				GUI.BeginGroup (r, (GUIStyle)"box");
+//				GUI.SetNextControlName ("note");
+//				currentNote.text = GUILayout.TextArea (currentNote.text, GUILayout.Width (100), GUILayout.Height (200));
+//				GUI.FocusControl ("note");
+//				GUI.EndGroup ();
 			}
 		}
+	}
+
+	void OnDrawGizmos ()
+	{
+		// Draw note icons, and highlight the one that has focus
 	}
 	
 	Vector2 WorldToGUIPoint (Vector3 position)
